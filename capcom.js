99eba603a16b796d34378055b2a6bcfab39198fb
tenswo <skt11992@daum.net>
2023-01-17 17:21:34 +0900

    123
    31231

---
+++ capcom.js
@@ -0,0 +1,17 @@
+// 오토슬라이드
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
