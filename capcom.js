ea744c7c76e06fbdd23156ac3dfa73d1843fe488
tenswo <skt11992@daum.net>
2023-01-20 16:13:56 +0900

    ad
    as

--- capcom.js
+++ capcom.js
@@ -1,17 +1,21 @@
 // 오토슬라이드
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// 이벤트 슬라이드
+$(function(){
+  $('.event').slick();
 });
